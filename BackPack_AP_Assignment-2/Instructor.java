f7a2e39c65178cefb6ba277044fdafbb48955aa5
Sidhant20475 <sidhant20475@iiitd.ac.in>
2021-10-19 13:26:17 +0530

    Assignment-2

---
+++ BackPack_AP_Assignment-2/Instructor.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Instructor {
+    private String ID;
+    Instructor(String id) {
+        this.ID=id;
+    }
+
+    public String get_ID() {
+        return ID;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
